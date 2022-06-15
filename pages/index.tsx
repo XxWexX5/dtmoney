b27dcfc41c5f7bbae7a648d18e74b6a9371d1485
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-06-15 17:57:54 -0300

    feat(Clean): Cleanning the structure.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Head>
+        <title>dt money</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Organize now your finance life." />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="author" content="Wevisonn R." />
+        <meta property="og:image" content="/images/money.png" />
+        <link rel="icon" href="/images/favicon.png" />
+      </Head>
+    </div>
+  );
 };
 
 export default Home;
