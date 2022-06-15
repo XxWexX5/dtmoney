660496e0289941a7b4bfbeb5237ec8b750b2973e
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-06-15 18:04:57 -0300

    feat(Meta): Adding meta tags.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,21 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>dt money</title>
+        <meta property="og:title" content="dt money" />
+        <meta
+          property="og:description"
+          content="Organize now your finance life."
+        />
+        <meta
+          property="og:url"
+          content="https://dtmoney-zeta-gold.vercel.app"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Organize now your finance life." />
         <meta name="keywords" content="HTML, CSS, JavaScript" />
         <meta name="author" content="Wevisonn R." />
         <meta property="og:image" content="/images/money.png" />
+        <meta property="og:type" content="article" />
         <link rel="icon" href="/images/favicon.png" />
       </Head>
     </div>
